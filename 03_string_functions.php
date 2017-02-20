e0a3cbea7fecb64aba3a7f5f6cb0587bb065ea05
BertA-immalle.be <bert.anthonissen@immalle.be>
2017-02-20 09:29:43 +0100

    oef 04 done

--- 03_string_functions.php
+++ 03_string_functions.php
@@ -4,8 +4,12 @@ $zin = "Binnenkort is het weeral lente";
 
 // Print af hoeveel woorden $zin bevat.
 
+echo str_word_count($zin);
+echo "<br>";
 
 // Vervang in $zin "lente" door "zomer" en druk af.
 
+$zin = "Binnenkor is het weeral zomer";
+echo $zin;
 
 ?>
