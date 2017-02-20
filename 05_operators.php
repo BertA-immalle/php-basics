5a6d9443c810bb3cf7fe60983557a185754cd5e9
BertA-immalle.be <bert.anthonissen@immalle.be>
2017-02-20 09:38:07 +0100

    oef 06 done

--- 05_operators.php
+++ 05_operators.php
@@ -2,9 +2,17 @@
 
 // Bereken de rest bij gehele deling van 10 door 3.
 
+$a = 10;
+$b = 3;
+$c = $a % $b;
+echo $c;
+echo "<br>";
+
 
 // Bereken de 5e macht van 2.
 
+echo pow(2, 5);
+echo "<br>";
 
 
 $var_a = "123";
@@ -16,7 +24,7 @@ if(true) {
     echo '$var_a en $var_b zijn aan elkaar gelijk maar zijn niet van hetzelfde type.';
 }
 
-if(true) {
+if(false) {
     echo '$var_a en $var_b zijn aan elkaar gelijk en van hetzelfde type.';
 }
 
