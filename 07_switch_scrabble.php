b174e936c1330e408915bca0f1745efea7432800
BertA-immalle.be <bert.anthonissen@immalle.be>
2017-02-23 15:11:32 +0100

    oef 8 done

--- 07_switch_scrabble.php
+++ 07_switch_scrabble.php
@@ -15,5 +15,32 @@ $punten = 0;
 // I 1 punt
 // N 1 punt
 
+for($i = 0; $i < strlen($woord); $i++) {  
+    echo $woord[$i]; 
+    switch(strtoupper($woord[$i])) {
+        case "Q":
+            $punten += 10;
+            break;
+        case "U":
+            $punten += 4;
+            break;
+        case "T":
+        case "R":
+            $punten += 2;
+            break;
+        case "I":
+        case "N":
+        case "E":
+        case "O":
+            $punten += 1;
+            break;
+        case "G":
+            $punten += 3;
+            break;  
+        
+    }
+    
+}
+echo "<br> $punten <br>";
 
 ?>
