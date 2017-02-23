eb09da5218d390d884691a5d26247c6e0512c7ea
BertA-immalle.be <bert.anthonissen@immalle.be>
2017-02-23 15:11:46 +0100

    oef 9 done

--- 08_dictionary_scrabble.php
+++ 08_dictionary_scrabble.php
@@ -34,19 +34,38 @@ $scrabble = array(
 
 // gevraagd: Zorg dat de functie show alle keys en values afdrukt. Maak gebruik van een foreach-loop. Extra: zorg dat de volgorde oplopend is naar aantal punten per letter.
 
-funcion show($assray) {
-
+function show($assray) {
+    echo "<ul>";
+    asort($assray); // sorteer value ascending (andere: arsort, ksort, krsort)
+    foreach($assray as $key => $val) {
+        echo "<li>" . $key . " => " . $val . "</li>";
+    }
+    echo "</ul>";
 }
 
 
 // gevraagd: Vul de functie verder aan zodat ze het totaal behaalde punten returnt. De code kan sterk vereenvoudigd (i.v.m. switch/case) door op de gepaste manier gebruik de maken van bovenstaande associatieve array.
 
 function punten($woord) {
-
-    return 0;
+    global $scrabble; // http://php.net/manual/en/language.variables.scope.php
+    $punten = 0;
+    echo "<p>We zoeken de punten van het woord: $woord</p>";
+    echo "<ul>";
+    for($i=0; $i<strlen($woord); $i++) {
+        $letter = strtoupper($woord[$i]); // in $scrabble zitten enkel hoofdletters, dus converteer
+        $letterpunt = $scrabble[$letter];
+        echo "<li>De letter $letter is $letterpunt punten waard.</li>";
+        $punten += $letterpunt;
+    }
+    echo "</ul>";
+    return $punten;
 }
 
 // gevraagd: Print van enkele woorden de punten af met bovenstaande functie.
 // Zorg dat er woorden met zowel kleine als hoofdletters tussen zitten.
+
+$woord = "QUATAR";
+
+echo punten($woord);
 
 ?>
